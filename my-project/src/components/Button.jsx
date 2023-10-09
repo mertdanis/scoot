bd030fd536e2ce4b6ee7250e1aef67863e64f7c5
M. D <mertdanis@outlook.com>
2023-10-09 16:37:21 +0300

    update

--- my-project/src/components/Button.jsx
+++ my-project/src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ children }) {
   return (
-    <button className="bg-yellow p-3 h-[53px] font-space-mono text-white">
+    <button className="bg-yellow p-3 h-[53px] w-[180px]  font-space-mono text-white">
       {children}
     </button>
   );
--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -12,23 +12,29 @@ function Hero() {
       }}
       className=" relative 	 text-white"
     >
-      <div className="flex flex-col gap-6 pt-[153px] px-[165px]">
-        <h2 className="text-4xl">Scooter sharing made simple</h2>
-        <p>
-          Scoot takes the hassle out of urban mobility. Our bikes are placed in
-          convenient locations in each of our cities. Use our app to locate the
-          nearest bike, unlock it with a tap, and you’re away!
-        </p>
-        <Button>Get Scootin</Button>
+      <div className="flex flex-col gap-6 py-[173px] ">
+        <h1 className="text-h1 w-[60%] leading-[56px] px-[165px]">
+          Scooter sharing made simple
+        </h1>
+        <div className="px-[260px]">
+          <p className="w-[44%] leading-[25px]">
+            Scoot takes the hassle out of urban mobility. Our bikes are placed
+            in convenient locations in each of our cities. Use our app to locate
+            the nearest bike, unlock it with a tap, and you’re away!
+          </p>
+          <div className="pt-[37px]">
+            <Button>Get Scootin</Button>
+          </div>
+        </div>
       </div>
 
       <img
-        className="absolute left-0 top-70"
+        className="absolute left-0 top-[354px]"
         src="/src/assets/patterns/line.svg"
         alt="line"
       />
       <img
-        className="absolute right-44 top-70"
+        className="absolute left-[705px] top-[354px]"
         src="/src/assets/patterns/right-arrow.svg"
         alt="line"
       />
